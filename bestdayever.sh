d82c002b200d4b5c8b30ef405c5e1cfe59bf403f
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2023-01-09 10:16:26 +0300

    working on linux command

---
+++ bestdayever.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo 'what is your name?'
+
+read name
+
+echo 'Good Mornning $name'
+
+sleep 1
+
+echo 'You are Looking Good $name'
+
+sleep 1
+
+echo '$name ,You have no beard though'
---
+++ himom.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+echo 'hi Mom'
+ 
+sleep 3
+
+echo 'Uh uh'
+
+sleep 3
+
+echo 'Oh wow'
+
+sleep 3
+
+echo 'I cant Believe that'
+
+sleep 3
+
+echo 'Bye, love you too'
+
